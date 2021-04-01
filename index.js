dca2b140316696fac20bfd2d4ebde0667c016ad9
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-04-01 20:57:04 +0600

    upload products with image link to database

--- index.js
+++ index.js
@@ -16,16 +16,21 @@ app.get('/', (req, res) => {
     res.send('Welcome to Fashion World server!')
 })
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2uohe.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     console.log('connection error:', err);
     const productCollection = client.db("eCommerceDB").collection("products");
-    console.log('database connection successfully')
+
+    app.post('/addProduct', (req, res) => {
+        const newProduct = req.body;
+        console.log('adding new product: ', newProduct);
+        productCollection.insertOne(newProduct)
+            .then(result => {
+                console.log('inserted count ', result.insertedCount)
+                res.send(result.insertedCount > 0)
+            })
+    })
 });
 
 
