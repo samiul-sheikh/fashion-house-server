8ad4bf65e8d72899dfe1cb64ea72bf522d074f71
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-04-01 22:16:04 +0600

    display product information from database in the client side

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ const port = process.env.PORT || 8000
 app.use(cors());
 app.use(bodyParser.json());
 
-console.log(process.env.DB_USER);
-
 app.get('/', (req, res) => {
     res.send('Welcome to Fashion World server!')
 })
@@ -22,6 +20,7 @@ client.connect(err => {
     console.log('connection error:', err);
     const productCollection = client.db("eCommerceDB").collection("products");
 
+    // product added in database
     app.post('/addProduct', (req, res) => {
         const newProduct = req.body;
         console.log('adding new product: ', newProduct);
@@ -31,8 +30,16 @@ client.connect(err => {
                 res.send(result.insertedCount > 0)
             })
     })
-});
 
+    // display products from database
+    app.get('/products', (req, res) => {
+        productCollection.find()
+            .toArray((err, products) => {
+                res.send(products)
+                console.log('from database', products)
+            })
+    })
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
